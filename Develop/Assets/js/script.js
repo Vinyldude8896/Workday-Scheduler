567f23664aac52ea3d88c8650222f4fc1d823f76
Kevin Reid <kevinnivek@me.com>
2022-06-01 13:19:03 -0400

    commiting partial working feature

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,3 +1,53 @@
-
+var tasks = {};
 
 $('#today-date').text(new Date());
+
+
+
+
+
+// var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     if (!tasks) {
+//       tasks = {
+//         toDo: [],
+//         inProgress: [],
+//         inReview: [],
+//         done: []
+//       };
+//     }
+//      // loop over object properties
+//   $.each(tasks, function(list, arr) {
+//     console.log(list, arr);
+//     // then loop over sub-array
+//     arr.forEach(function(task) {
+//       createTask(task.text, task.date, list);
+//     });
+//   });
+// };
+
+    //save Tasks function
+    var saveTasks = function() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+
+    // on input blur save text value
+    $(".input").on("blur", function(){
+        var text = $(this).val();
+         console.log(text);
+
+        });
+
+
+  $(".btn").on("click", function() {
+     tasks.push({
+         text: $(this)
+         .find("input")
+         .text()
+         .trim()
+     })
+     console.log(tasks);
+      saveTasks();
+  });
