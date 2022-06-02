9d695bb0300c92acb69dc540f5110d1cecdf465d
Kevin Reid <kevinnivek@me.com>
2022-06-02 11:19:08 -0400

    commiting new home page franmework

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,34 +1,16 @@
-var tasks = {
-    time: [],
-    tasktext: [],
-
-};
+var tasks = {};
 
 $('#today-date').text(new Date());
 
 
-var loadTasks = function() {
-    savedTasks = JSON.parse(localStorage.getItem("tasks"));
-  
-    // if nothing in localStorage, create a new object to track all task status arrays
-    if (!tasks) {
-      tasks = {
-        time: [],
-        tasktext: [],
-      };
-    }
-  
-    // loop over object properties
-    $.each(tasks, function(time, tasktext) {
-      console.log(time, tasktext);
-      // then loop over sub-array
-      arr.forEach(function(task) {
-        createTask(tasks.time, tasks.tasktext);
-      });
-    });
-  };
-  
+// var loadTasks = function() {
+//    var savedtimes = JSON.parse(localStorage.getItem(tasks.time));
+//    console.log("These are the saved times " + savedtimes);
 
+  
+    $(function() {
+        $("#sortable").sortable();
+    });
 
 
     //save Tasks function
@@ -36,28 +18,24 @@ var loadTasks = function() {
         localStorage.setItem("tasks", JSON.stringify(tasks));
     };
 
-    // on input blur save text value
-    $(".input").on("blur", function(){
-        var text = $(this).val();
-        //  console.log(text);
+    // task text was clicked
+    $(".list-group").on("click", "li", function() {
+    // get current text of li element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
 
-        });
-
-
-
-  // on click of button next to checkbox
-  $(".btn").on("click", function() {
-    var buttonID = $(this).attr("id")
-     console.log(buttonID);
-
-    var InputTextBox = $(this).parent().prev().val();
-     console.log(InputTextBox);
-
-     tasks['time'].push(buttonID);
-     tasks['tasktext'].push(InputTextBox);
-
-     saveTasks();
   });
 
+
+
+
     // load tasks for the first time
-  loadTasks();
+//   loadTasks();
